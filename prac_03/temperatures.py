7a73c972e956d5aa43362f06a45abcb3c03b40c2
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-08-20 20:29:28 +1000

    Add comments to temperatures.py

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -30,11 +30,13 @@ def main():
 
 
 def fahrenheit_to_celsius(fahrenheit):
+    """Convert fahrenheit to celsius"""
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
 
 
 def celsius_to_fahrenheit(celsius):
+    """Convert celsius to fahrenheit"""
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
 
