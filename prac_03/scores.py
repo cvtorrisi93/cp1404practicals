f022da5902bccfb3e564f8c45499136f0d5fcf2a
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-08-21 22:50:37 +1000

    Add function to produce random score and add a for loop to print the score and result

--- prac_03/scores.py
+++ prac_03/scores.py
@@ -3,32 +3,46 @@ CP1404/CP5632 - Practical
 Scores program to determine what the score is based on value
 """
 
+import random
+
+MIN_SCORE = 0
+MAX_SCORE = 100
+
 
 def main():
 
+    number_of_scores = get_valid_integer("Enter a number of scores to generate: ")
+
+    for i in range(number_of_scores):
+        score = get_random_score()
+        result = determine_score(score)
+        print("{:<2} is {}".format(score, result))
 
 
-def get_valid_integer():
+def get_random_score():
+    """Produces a random int between the minimum and maximum score"""
+    score = random.randint(MIN_SCORE, MAX_SCORE)
+    return score
+
+
+def get_valid_integer(prompt):
     """Get valid integer from user"""
     valid_input = False
     while not valid_input:
         try:
-            number = int(input("Enter a score:"))
+            number = int(input(prompt))
             valid_input = True
         except ValueError:
             print("Error: not a valid integer")
     return number
 
 
-
 def determine_score(score):
     """Computes the score"""
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
+    if score >= 90:
         return "Excellent"
     elif score >= 50:
-        return "Pass"
+        return "Passable"
     else:
         return "Bad"
 
