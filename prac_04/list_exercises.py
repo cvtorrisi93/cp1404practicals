65fc68c3f572409573dd2f4203e047664ebbd63f
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-08-29 09:50:44 +1000

    Edit list exercise file to add function for woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,10 +3,25 @@ CP1404/CP5632 Practical - Christian Torrisi
 List exercises
 """
 
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
 
 def main():
     numbers = get_numbers()
     print_number_outputs(numbers)
+
+    username = input("Username: ")
+
+    check_username_access(username)
+
+
+def check_username_access(username):
+    """Check if user input matches any element in USERNAMES list"""
+    if username in USERNAMES:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 
 def print_number_outputs(lst):
@@ -17,10 +32,10 @@ def print_number_outputs(lst):
     print("The average of numbers is {}".format(sum(lst) / len(lst)))
 
 
-def get_numbers():
-    """Gets 5 number from the user"""
+def get_numbers(x=5):
+    """Gets numbers based on above parameter from the user"""
     numbers = []
-    for i in range(5):
+    for i in range(x):
         number = int(input("Number: "))
         numbers.append(number)
     return numbers
