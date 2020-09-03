8f7ce092d60cfba1a18ade69dbce1a607761b9b9
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-09-03 14:30:18 +1000

    Add main function

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,24 +5,30 @@ Using dictionaries
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
+
+def main():
+
+    print(CODE_TO_NAME)
+
     state_code = input("Enter short state: ").upper()
+    while state_code != "":
+        if state_code in CODE_TO_NAME:
+            print(state_code, "is", CODE_TO_NAME[state_code])
+        else:
+            print("Invalid short state")
+        state_code = input("Enter short state: ").upper()
+
+    max_code_length = get_code_max_length()
+
+    for state_short, state_long in CODE_TO_NAME.items():
+        print("{:{max_code_length}} is {}".format(state_short, state_long, max_code_length=max_code_length))
 
 
 def get_code_max_length():
-    codes = [code for code in CODE_TO_NAME]
-    max_length = len(max(codes, key=len))
+    states_short = [state_short for state_short in CODE_TO_NAME]
+    max_length = len(max(states_short, key=len))
     return max_length
 
 
-max_code_length = get_code_max_length()
-
-for code, name in CODE_TO_NAME.items():
-    print("{:{max_code_length}} is {}".format(code, name, max_code_length=max_code_length))
+main()
