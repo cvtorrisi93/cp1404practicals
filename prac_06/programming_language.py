124eab5dc82e07be4eb4e09cf807b53f3747d220
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-09-09 17:26:19 +1000

    Add languages file. Amend is_dynamic function

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,19 +1,19 @@
 """
 CP1404/CP5632 Practical - Christian Torrisi
-Intermediate Exercise - Programming Language
+Intermediate Exercise - Programming Language Class
 """
 
 
 class ProgrammingLanguage:
-    """Representation of a programming language object"""
+    """Representation of a ProgrammingLanguage object"""
 
-    def __int__(self, name="", typing="", reflection=False, year=0):
-        """Initialise a programming language instance
+    def __init__(self, name="", typing="", reflection=False, year=0):
+        """Initialise a ProgrammingLanguage instance
 
-        name: string, name of the programming language
-        typing: string, static or dynamic typing
-        reflection: boolean
-        year: int, year the programming language was first released
+        name:       string, name of the programming language
+        typing:     string, static or dynamic typing
+        reflection: boolean, whether the programming language reflects or not
+        year:       int, year the programming language was first released
         """
 
         self.name = name
@@ -21,9 +21,15 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        """Returns a string output for a ProgrammingLanguage object"""
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection,
+                                                                           self.year)
+
     def is_dynamic(self):
+        """Check if the ProgrammingLanguage instance has dynamic typing"""
         is_dynamic = False
-        if self.typing:
+        if self.typing == "Dynamic":
             is_dynamic = True
 
         return is_dynamic
