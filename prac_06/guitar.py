c56f563d1a21caa2ff57b2b03da399f806a9684a
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-09-10 15:38:10 +1000

    Add guitar test file with tests. Add guitars file

---
+++ prac_06/guitar.py
@@ -0,0 +1,31 @@
+"""
+CP1404/CP5632 Practical - Christian Torrisi
+Do-from-Scratch - Guitars!.
+"""
+
+CURRENT_YEAR = 2020
+
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance.
+
+        name: string, name of the guitar
+        year: int, the year the guitar was released
+        cost: float, the cost of the guitar
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """ """
+        print("{} ({}) : ${:.2f}".format(self.name, self.year, self.cost))
+
+    def get_age(self):
+        """Gets the age of the guitar from the current year"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= 50
---
+++ prac_06/guitars_test.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical - Christian Torrisi
+Do-from-Scratch - Guitars! - Test!.
+"""
+
+from prac_06.guitars import Guitar
+
+
+gibson_l5_ces = Guitar("Gibson L-5 CES", 1922, 16035.40)
+another_guitar = Guitar("Another Guitar", 2013, 10000.009)
+
+print("{} get_age() - Expected 98. Got {}".format(gibson_l5_ces.name, gibson_l5_ces.get_age()))
+print("{} get_age() - Expected 7. Got {}".format(another_guitar.name, another_guitar.get_age()))
+print("{} is_vintage() - Expected True. Got {}".format(gibson_l5_ces.name, gibson_l5_ces.is_vintage()))
+print("{} is_vintage() - Expected False. Got {}".format(another_guitar.name, another_guitar.is_vintage()))
