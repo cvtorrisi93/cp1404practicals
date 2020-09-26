7a6ec6f321d3f2988821b400c1c056f9cdb03573
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-09-26 23:50:00 +1000

    Add all functionality to complete exercise (not expected solution)

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -6,16 +6,37 @@ Do-from-scratch - Converting Miles to Kilometres
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.app import StringProperty
+
+CONVERSION_RATIO = 1.60934
 
 
 class ConvertMilesToKm(App):
     """ A kivy app that converts miles to kilometres"""
+    output_message = StringProperty()
 
     def build(self):
-        Window.size = (500, 300)
+        """Builds the kivy app from the kv file"""
+        Window.size = (400, 200)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_conversion(self, miles):
+        """Converts miles to kilometres when the button is pressed"""
+        try:
+            miles = float(miles)
+            kilometres = float(miles) * CONVERSION_RATIO
+        except ValueError:
+            kilometres = 0
+        self.output_message = "{:.3f}".format(kilometres)
+
+    def handle_increment(self, value):
+        """Increases or decreases the test input value based on the button pressed"""
+        if self.root.ids.input_number.text == '':
+            self.root.ids.input_number.text = str(0)
+        new_value = int(self.root.ids.input_number.text) + value
+        self.root.ids.input_number.text = str(new_value)
 
 
 ConvertMilesToKm().run()
