59f692edb3269d93963c6846ed48676b8950f044
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-10-09 21:24:48 +1000

    Edit unreliable car file. Create unreliable car test file

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -1,6 +1,6 @@
 """
 CP1404/CP5632 Practical - Christian Torrisi
-Walkthrough - Taxi class
+Intermediate - Taxi class
 """
 
 from random import randint
@@ -11,16 +11,14 @@ class UnreliableCar(Car):
     """Specialised version of a Car that that may be unreliable to drive."""
 
     def __init__(self, name, fuel, reliability):
+        """Initialise a Unreliable Car instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
-        number = randint(0, 100)
-        if number < self.reliability:
-            distance_driven = super().drive(distance)
-            print(number, " ", distance_driven)
-        else:
-            distance_driven = super().drive(0)
-            print(number, " ", distance_driven)
+        """Drive a certain distance, or don't drive, based on the reliability"""
+        if randint(0, 100) >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
 
         return distance_driven
---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical - Christian Torrisi
+Intermediate - Taxi class
+"""
+
+from prac_08.unreliable_car import UnreliableCar
+
+the_bomb = UnreliableCar("The Bomb", 100, 20)
+print(the_bomb)
+the_bomb.drive(50)
+print(the_bomb)
