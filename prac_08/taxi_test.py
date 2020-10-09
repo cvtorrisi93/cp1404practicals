84c619aed6ee941797ccb5622b22e1807eb58e9a
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-10-09 19:12:11 +1000

    Add content to test the taxi class

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,3 +2,17 @@
 CP1404/CP5632 Practical - Christian Torrisi
 Walkthrough - Test Taxi
 """
+
+from prac_08.taxi import Taxi
+
+prius_01 = Taxi("Prius 1", 100, 1.23)
+
+prius_01.drive(40)
+
+print(prius_01)
+print("Current fare is $", prius_01.get_fare())
+
+prius_01.start_fare()
+
+print(prius_01)
+print("Current fare is $", prius_01.get_fare())
