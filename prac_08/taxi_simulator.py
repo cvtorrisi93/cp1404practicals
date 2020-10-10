0913ec9ec3126a5c2ec027438862b197c11f4eae
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2020-10-10 17:36:26 +1000

    Complete taxi simulator exercise

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -12,6 +12,7 @@ MENU = "q)uit, c)hoose, d)rive"
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
+    total_bill = 0
 
     print("Let's drive")
     print(MENU)
@@ -22,12 +23,28 @@ def main():
             show_taxis(taxis)
             taxi_choice = int(input("Choose taxi: "))
             current_taxi = taxis[taxi_choice]
+            print("Bill to date: ${:.2f}".format(total_bill))
         elif menu_choice == "d":
-            pass
+            current_taxi.start_fare()
+            distance = int(input("Drive how far? "))
+            current_taxi.drive(distance)
+            current_bill = current_taxi.get_fare()
+            total_bill += current_bill
+            print("Your {} trip cost you ${:.2f}".format(current_taxi.name, current_bill))
+            print("Bill to date: ${:.2f}".format(total_bill))
+        else:
+            print("Invalid choice")
 
+        print(MENU)
+        menu_choice = input(">>>").lower()
+
+    print("Total trip cost: ${:.2f}".format(total_bill))
+    print("Taxis are now:")
+    show_taxis(taxis)
 
 
 def show_taxis(taxis):
+    """Show all taxis that are currently in the list"""
     for number, taxi in enumerate(taxis):
         print("{} - {}".format(number, taxi))
 
